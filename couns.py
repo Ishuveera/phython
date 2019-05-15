0f5eb4ba63ef759445dd63c5f04400979c4bec93
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-05-14 22:40:09 -0700

    Rename coun.py to couns.py

---
+++ couns.py
@@ -0,0 +1,5 @@
+ch=input("enter the letters")
+if ch='a' or ch='A' or ch='e' or ch='E' or ch='i' or ch='I' or ch='o' or ch='O' OR ch='u' or ch='U':
+print("letter is vowels")
+else:
+print("letter is counsonant")
