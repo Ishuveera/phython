6fd3fed51f25b8a736ad9fa5f7339a1459139de4
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-05-14 20:32:36 -0700

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,8 @@
+print("enter '0' for exist);
+ch= input(enter the alphabet:");
+if(ch==0):
+  exit();
+if(ch>='a'&&ch<='z')or(ch>='A'&&ch<='Z'):
+ print(ch,is an alphabet);
+ else:
+ printf9ch, is not an alphabet);
