b311c9dda65ba6339aac3414b3cf852b37adbcd8
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-05-14 23:02:22 -0700

    Create large.py

---
+++ large.py
@@ -0,0 +1,12 @@
+num1=10
+num2=8
+num3=5
+num=float(input("enter the frst num:"))
+num=float(input("enter the secod num:"))
+num=float(input("enter the third num:"))
+if num1>=num2 or num1>=num3
+print("num1 largest")
+if num2>=num1 or num2>=num3
+print("num2 largest")
+else
+print("numj3 largest)
