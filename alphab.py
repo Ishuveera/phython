5cd2fa4511c37a712ae3ed15fd05fdea69fb5614
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-05-14 20:33:46 -0700

    Rename alpha.py to alphab.py

---
+++ alphab.py
@@ -0,0 +1,8 @@
+print("enter '0' for exist);
+ch= input(enter the alphabet:");
+if(ch==0):
+  exit();
+if(ch>='a' and ch<='z')or(ch>='A' and ch<='Z'):
+ print(ch,is an alphabet);
+ else:
+ printf9ch, is not an alphabet);
