546d5bdd4fc3d807eac183a9ea9101728fb449c9
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-05 17:30:18 +0530

    Create reapect.py

---
+++ reapect.py
@@ -0,0 +1,11 @@
+def printTwoElements (arr,size):
+for i in range (size):
+if arr[abs (arr[i])-1]>0:
+  arr[abs(arr[i])-1] = -arr[abs(arr[i]-1]
+  else:
+  print("the repeacting element is",abs(arr[i]))
+  for i in range(size):
+  if arr[i] > 0:
+  print('and the unique element is",i+1)
+  n=len(arr)
+  print two element(arr,n)
