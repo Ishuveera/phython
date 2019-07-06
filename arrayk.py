740fa52256bd0707b97477292f96405f12aea0cf
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-06 11:05:57 +0530

    Create arrayk.py

---
+++ arrayk.py
@@ -0,0 +1,12 @@
+def has twocandidates(A,arr_size-1)
+quicksort(A,o,arr_size-1)
+1=0
+r=arr_size-1
+while 1<r:
+  if(a[1]+a[r]==sum):
+  return 1
+  elif (a[1]+a[r]<sum):
+  1+=1
+  else:
+  r -=1
+  return 0
