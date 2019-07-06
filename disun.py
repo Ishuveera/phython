650f82349a243b99329934ec21881cd53571a383
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-06 10:45:43 +0530

    Create disun.py

---
+++ disun.py
@@ -0,0 +1,11 @@
+def printdistinct(arr,n):
+for i in range(0,n):
+d=0
+for j in range(0,i):
+if(arr[i]==arr[j]):
+break
+if(d==0):
+print(arr[i])
+arr=[6,10,5,4,9,120,4,6,10]
+n=len(arr)
+printDistinct(arr,n)
