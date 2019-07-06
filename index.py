8ae606f2136cbf5e986d96f3fa464d10ca8d35bc
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-06 09:43:14 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,14 @@
+def findpos(a,k)
+l,h,val,0,1,arr[0]
+while val<key:
+1=h
+h=2*h
+val=arr[h]
+return binary searc
+h[a,l,h,key]
+arr=[35,45,10,90,80]
+ans=findpos(arr,1o)
+if ans == -1:
+  print "element is not found"
+ else:
+  print "element is found at index",ans
