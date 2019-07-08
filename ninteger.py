0d19818dc0362bbb0a76e9452282ac41d3740ea1
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-08 06:17:06 -0700

    Create ninteger.py

---
+++ ninteger.py
@@ -0,0 +1,8 @@
+def findnnumber(A,arr_size,sum):
+for i in range(0,arr_size-1):
+for j in range(i+1,arr_size-1):
+for k in range(j+1,arr_size-1):
+if a[i]+a[j]+a[k]==sum
+print("integer is ,",",A[i],"",A[j],","A[k])
+return true 
+return false
