78f2fbed077ae4b7a304e078d499a5235b345579
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-19 12:30:48 -0700

    Update pro4.py

--- pro4.py
+++ pro4.py
@@ -1,13 +1,11 @@
-TH=int(input())
-SH=list(map(int,input().split()))
-A=0
-B=0
-SH.sort(reverse=True)
-for X in SH:
-  SH=A+X
-  if B>SH:
-    A=SH
-  else:
-    A=B
-    B=SH
-print(A,B)
+A1,B1=map(str,input().split())
+C1=0
+if len(A1)>len(B1):
+    A1,B1=B1,A1
+D1=0
+while D1<len(A1):
+      C1+=(ord(B1[D1])-ord(A1[D1]))
+      D1+=1
+for D1 in range(D1,len(B1)):
+      C1+=ord(B1[D1])-ord('a')+1
+print(C1)
