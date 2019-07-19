91a389b3932e560f378c97382bf9bf94b3ced7cd
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-19 06:26:46 -0700

    Update pro56.py

--- pro56.py
+++ pro56.py
@@ -1,8 +1,10 @@
-N,K = input().split()
-N,K = int(N),int(K)
-l = [ int(X) for X in input().split()]
-#print(N,K, l)
-for Y in range(0,N) :
-    if (86400-l[Y]) >= K :
-        print(Y+1)
-    K -= (86400-l[Y])
+S,P=map(int,input().split())
+N=list(map(int,input().split()))
+C=0
+for X in N:
+    K=86400-X
+    P-=K
+    C+=1
+    if P<=0:
+        break  
+print(C)
