5908eb5b5c9438981e0e74bfba1a4dd47fcd1f69
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-19 12:48:09 -0700

    Update pro8.py

--- pro8.py
+++ pro8.py
@@ -1,11 +1,11 @@
 import math
-AA1,Z1=map(int,input().split())
-CC1=[]
-BB1=list(map(int,input().split()))
-for Y in range(0,ZZ1):
-    L,H=map(int,input().split())
-    CC1.append([L,H])
-for Y in CC1:
-    nn=Y[0]-1
-    mm=Y[1]-1
-    print(math.gcd(BB1[nn],BB1[mm]))
+P1,Q1=map(int,input().split())
+N=[]
+V=list(map(int,input().split()))
+for X in range(0,Q1):
+        A,B=map(int,input().split())
+        N.append([A,B])
+for X in N:
+        x=X[0]-1
+        y=X[1]-1
+        print(math.gcd(V[x],V[y]))
