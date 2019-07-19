d506a2fb5e67554fbd122b8f0ad5c36b41ce50eb
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-19 12:58:33 -0700

    Update pro10.py

--- pro10.py
+++ pro10.py
@@ -1,8 +1,8 @@
 A12=int(input())
 B12=list(map(int,input().split()))
-C1=0
+D1=0
 for X in range(0,A12):
-    for Y in range(0,X):
-        if B12[p]<B12[X]:
-            C1=C1+B12[P]
-print(C1)
+	for P in range(0,X):
+		if B12[P]<B12[X]:
+			D1=D1+B12[P]
+print(D1)
