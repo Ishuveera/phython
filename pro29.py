03798dfcb6799398bb0574ee6e4c108208e28320
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-20 09:14:42 -0700

    Create pro29.py

---
+++ pro29.py
@@ -0,0 +1,15 @@
+Nn=int(input())
+L=[]
+M=len(str(Nn))
+S=0
+for _ in range(M):
+    S+=9
+for X in range(Nn-S,Nn):
+    R=0
+    D=i
+    while D>0:
+        R+=(D%10)
+        D=D//10
+    if R+X==Nn:
+        L.append(X)
+print(len(L),*L,sep='\n')
