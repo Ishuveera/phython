1cd958a335f612edf9009ef81988fef505623589
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2019-07-19 21:28:49 -0700

    Create PL5.py

---
+++ PL5.py
@@ -0,0 +1,7 @@
+A2=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+B2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+C2=str(input())
+for D1 in range(20):
+  if(C2==A2[D1]):
+    print(B2[D1])
+    break
